539372cb5c03bd58cc05a8baa0d6ab6f25eb7d60
Theo S <iosdeveloper@tgs.me.uk>
2016-03-21 12:20:11 +0000

    Implemented contacts interface

--- Contacts WatchKit Extension/InterfaceController.m
+++ Contacts WatchKit Extension/InterfaceController.m
@@ -7,6 +7,16 @@
 //
 
 #import "InterfaceController.h"
+#import <Contacts/Contacts.h>
+#import "ContactRow.h"
+
+static NSString  *const kContactRowIdent=@"ContactRowIdent";
+static NSString  *const kContacts=@"Contacts";
+static NSString  *const kLoading=@"Loading...";
+
+static NSString  *const kAnonymousPerson=@"AnonymousPerson";
+static NSString  *const kAnonymousCompany=@"AnonymousCompany";
+static NSString  *const kShowContactDetails=@"ShowContactDetails";
 
 
 @interface InterfaceController()
@@ -20,6 +30,7 @@
     [super awakeWithContext:context];
 
     // Configure interface objects here.
+    [self loadContacts];
 }
 
 - (void)willActivate {
@@ -31,6 +42,96 @@
     // This method is called when watch view controller is no longer visible
     [super didDeactivate];
 }
+- (void) loadContacts
+{
+    CNContactStore *store=[[CNContactStore alloc] init];
+    
+    [store requestAccessForEntityType:CNEntityTypeContacts
+                    completionHandler:^(BOOL granted, NSError * _Nullable error)
+     {
+         if (granted == YES)
+         {
+            [self setTitle:kLoading];
+             NSArray *keys = @[CNContactFamilyNameKey,
+                               CNContactGivenNameKey,
+                               CNContactPostalAddressesKey,
+                               CNContactOrganizationNameKey,
+                               CNContactTypeKey
+                               ];
+             NSString *containerId = store.defaultContainerIdentifier;
+             NSPredicate *predicate = [CNContact predicateForContactsInContainerWithIdentifier:containerId];
+             
+             
+             NSError *error;
+             NSArray *allContacts = [store unifiedContactsMatchingPredicate:predicate keysToFetch:keys error:&error];
+             if (error)
+             {
+                 NSLog(@"error fetching contacts %@", error);
+            }
+             else
+             {
+                 for (CNContact *object in allContacts)
+                 {
+                     if ([object isKeyAvailable:CNContactPostalAddressesKey])
+                     {
+                         if (object.postalAddresses.count>0)
+                         {
+                             [self addContactToTable:object];
+                         }
+                     }
+                 }
+            }
+          
+         }
+         else
+         {
+             NSLog(@"Permission not granted");
+         }
+         [self setTitle:kContacts];
+     }];
+    
+}
+
+-(void) addContactToTable:(CNContact *)contact
+{
+    
+    NSInteger newRowIndex=self.tableView.numberOfRows;
+    NSIndexSet *newRowIndexSet=[NSIndexSet indexSetWithIndex:newRowIndex];
+    
+    [self.tableView insertRowsAtIndexes:newRowIndexSet withRowType:kContactRowIdent] ;
+    ContactRow *row=[self.tableView rowControllerAtIndex:newRowIndex] ;
+    
+    NSString *fullName=nil;
+    NSString *orgName=nil;
+    UIImage *Image=[UIImage imageNamed:kAnonymousPerson];
+    
+    if (contact.contactType==CNContactTypePerson)
+    {
+        fullName=[[NSString alloc] initWithFormat:@"%@ %@", contact.givenName, contact.familyName];
+        orgName=contact.organizationName;
+    }
+    else if  (contact.contactType==CNContactTypeOrganization)
+    {
+        fullName=contact.organizationName;
+        orgName=nil;
+        {
+            Image=[UIImage imageNamed:kAnonymousCompany];
+        }
+    }
+    
+    [row.name setText:fullName];
+    [row.contactPicType setImage:Image];
+    row.identifier=contact.identifier;
+    
+}
+
+
+- (void)table:(WKInterfaceTable *)table didSelectRowAtIndex:(NSInteger)rowIndex
+{
+    ContactRow *row=[self.tableView rowControllerAtIndex:rowIndex] ;
+    [self presentControllerWithName:kShowContactDetails context:row.identifier];
+}
+
 
 @end
 
